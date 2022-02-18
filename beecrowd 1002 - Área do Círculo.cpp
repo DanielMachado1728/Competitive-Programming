2819baca237afdad6167c96eeeee799fd153bfcd
DanielMachado1728 <54379990+DanielMachado1728@users.noreply.github.com>
2022-02-18 12:02:47 -0300

    Create beecrowd 1002 - Área do Círculo.cpp

---
+++ beecrowd 1002 - Área do Círculo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+# define pi 3.14159
+
+int main() {
+ double raio, area;
+ cin >> raio;
+ area = pi*pow(raio,2);
+ cout.precision(4);
+ cout << "A=" << fixed << area << endl;
+ 
+ return 0;
+}
