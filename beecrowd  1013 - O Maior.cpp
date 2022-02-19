578d419d05fed2f62525e2db8ee806f050ce93ee
DanielMachado1728 <54379990+DanielMachado1728@users.noreply.github.com>
2022-02-18 23:15:59 -0300

    Create beecrowd  1013 - O Maior.cpp

---
+++ beecrowd  1013 - O Maior.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+  int a,b,c, maior1, maior2;
+  cin >> a >> b >> c;
+  maior1 = (a+b+abs(a-b))/2;
+  maior2 = (maior1+c+abs(maior1-c))/2;
+  cout << maior2 << " eh o maior" << endl;
+
+  return 0;
+}
