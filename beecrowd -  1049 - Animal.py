1da99f09e53e42dcd6116c6e6ee09606b6dd0afe
DanielMachado1728 <54379990+DanielMachado1728@users.noreply.github.com>
2022-02-23 14:49:12 -0300

    Create beecrowd -  1049 - Animal.py

---
+++ beecrowd -  1049 - Animal.py
@@ -0,0 +1,27 @@
+a = input()
+b = input()
+c = input()
+if a == "vertebrado":
+  if b == "ave":
+    if c == "carnivoro":
+      print ("aguia")
+    else:
+      print ("pomba")
+if a == "vertebrado":
+  if b == "mamifero":
+    if c == "onivoro":
+      print ("homem")
+    else:
+      print ("vaca")
+if a == "invertebrado":
+  if b == "inseto":
+    if c == "hematofago":
+      print ("pulga")
+    else:
+      print ("lagarta")
+if a == "invertebrado":
+  if b == "anelideo":
+    if c == "hematofago":
+      print ("sanguessuga")
+    else:
+      print ("minhoca")
